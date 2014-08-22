866f5861acbd8acde31f6efd9491b8d4d46d5e0c
Brendan <brendan@shellshockcomputer.com.au>
2014-08-22 21:13:18 +1000

    Added Api for blocks, accounts and leased

--- webserver.py
+++ webserver.py
@@ -3,7 +3,7 @@
 # author: brendan@shellshockcomputer.com.au
 
 import ConfigParser
-from bottle import route, install, run, template, static_file, response, PasteServer#, debug
+from bottle import route, install, run, template, static_file, response, PasteServer, debug
 from bottle_sqlite import SQLitePlugin
 import json
 import urllib
@@ -28,7 +28,30 @@ def blocktime():
     getdl = forging["deadline"]
     dl = str(datetime.timedelta(seconds=getdl))
     return {'blocktime': dl}
+
+@route('/api/accounts')
+def apiaccounts(db):
+    response.headers['Cache-Control'] = 'public, max-age=3600'
+    getlastheight = db.execute("SELECT height FROM blocks ORDER BY timestamp DESC").fetchone()
+    lastheight = getlastheight[0]
+    c = db.execute("SELECT account, heightfrom, heightto, amount FROM leased WHERE heightto > %s" % (lastheight)).fetchall()
+    accounts = json.dumps( [dict(ix) for ix in c], separators=(',',':'))   
+    return accounts
+
+@route('/api/blocks')
+def apiblocks(db):
+    response.headers['Cache-Control'] = 'public, max-age=3600'
+    c = db.execute("SELECT height, timestamp, totalfee FROM blocks WHERE totalfee > 0 ORDER BY timestamp DESC").fetchall()
+    blocks = json.dumps( [dict(ix) for ix in c], separators=(',',':'))
+    return blocks
+
+@route('/api/leased')
+def apileased():
+    getaccounts = json.loads(urllib2.urlopen(config.get("pool", "nhzhost")+"/nhz?requestType=getAccount&account="+config.get("pool", "poolaccount")).read())
+    leasebal = getaccounts['effectiveBalanceNHZ']
+    return {'blocktime': leasebal}   
     
+            
 @route('/')
 def default(db):
     response.headers['Cache-Control'] = 'public, max-age=3600'
@@ -42,8 +65,6 @@ def default(db):
     result = c.fetchall()
     e = db.execute("SELECT height, timestamp, totalfee FROM blocks ORDER BY timestamp DESC limit 5")
     block = e.fetchall() 
-    getaccounts = json.loads(urllib2.urlopen(config.get("pool", "nhzhost")+"/nhz?requestType=getAccount&account="+config.get("pool", "poolaccount")).read())
-    leasebal = getaccounts['effectiveBalanceNHZ']   
     output = template('default', pa=poolaccount, fee=poolfee, rows=result, blocks=block, nhzb=leasebal)
     return output
 
@@ -59,14 +80,8 @@ def get_favicon():
 
 @route('/accounts')
 def accounts(db):
-    response.headers['Cache-Control'] = 'public, max-age=3600'
-    db.text_factory = str
-    d = db.execute("SELECT height FROM blocks ORDER BY timestamp DESC")
-    getlastheight = d.fetchone()
-    lastheight = getlastheight[0]
-    c = db.execute("SELECT account, heightfrom, heightto, amount FROM leased")
-    result = c.fetchall()   
-    output = template('accounts', rows=result)
+    response.headers['Cache-Control'] = 'public, max-age=86400'   
+    output = template('accounts')
     return output
 
 @route('/blocks')
@@ -102,5 +117,5 @@ def paid(db):
     output = template('paid', rows=result)
     return output
 	
-#debug(True)    
+debug(True)    
 run(server=PasteServer, port=8810, host='0.0.0.0')
